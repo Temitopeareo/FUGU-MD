f06d60bfad615ef939f7e51cdd4138fe941ea9d3
Temitopeareo <132304443+Temitopeareo@users.noreply.github.com>
2024-05-16 12:56:39 +0100

    FUGU MD

--- commands/Tempmails.js
+++ commands/Tempmails.js
@@ -20,10 +20,10 @@ Object.defineProperty(exports, "__esModule", { value: true });
 const { france } = require("../framework/france");
 france({ nomCom: "tempmail", reaction: "😌", nomFichier: __filename }, async (dest, zk, commandeOptions) => {
     console.log("Commande saisie !!!s");
-    let z = '*FLASH-MD* Temporary emails,Powered by *France King* \n\n ' + "Click the links below to create unlimited TEMPORARY Emails. Powered by *FLASH-MD.*\n\n";
-    let d = ' 1️⃣ https://tempumail.com\n\n 2️⃣ https://etempmail.com\n\n 3️⃣ https://ghostmail.one\n\n 4️⃣ https://tempmailid.com';
+    let z = '*FUGU-MD* Temporary emails,Powered by *ABRO TECH* \n\n ' + "Click the link below to create unlimited TEMPORARY Emails. Powered by *FUGU-MD.*\n\n";
+    let d = ' 1️⃣ https://mymail.guru.com';
     let varmess = z + d;
-    var img = 'https://telegra.ph/file/f1f2303ff4e39b0a3b6b3.jpg';
+    var img = 'https://imgur.com/7doiWDc.jpg';
     await zk.sendMessage(dest, { image: { url: img }, caption: varmess });
     //console.log("montest")
 });
